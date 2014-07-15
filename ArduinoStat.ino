c0a92fee5e6070512665f0736ef06c5d1ce55272
Harcrow <tyler.jowaisas@gmail.com>
2014-07-14 23:31:50 -0400

    Update ArduinoStat.ino

--- ArduinoStat.ino
+++ ArduinoStat.ino
@@ -9,7 +9,6 @@ My hope is that this will simplify consuming less natrual gas.
 //code as possible, just to decrease over powerconsumption.
 //Ideally I'd like a couple 9V batteries to power this for the season.
 
-
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h>
 #include <Adafruit_MCP23017.h>
@@ -20,58 +19,50 @@ My hope is that this will simplify consuming less natrual gas.
 
 #define DHTPIN 2
 #define DHTTYPE DHT22
+#define bufferMax 24
+#define light_pin 0
+#define is_on 9
 
+byte bufferSize;
+char buffer[bufferMax];
+
+char cmd[15];    // Nothing magical about 15, increase these if you need longer commands/parameters
+char param1[15];
+char param2[15];
+String command = "";
+
+byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0x23, 0x06 };
+IPAddress ip(192, 168, 1, 177);
 
 DHT dht(DHTPIN, DHTTYPE);
 
-
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
+byte set_temp = 68;
+byte light_value;
+byte is_temp;
 
-byte light_pin = 0;
-int light_value;
-//declaring a pin for an LED, indicating status
-byte is_on = 9;
-int  set_temp = 68;
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] =
- {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
- IPAddress ip(192,168,1,177);
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use 
-// (port 80 is default for HTTP):
- EthernetServer server(80);
-
-int is_temp;
+EthernetServer server(80);
 
 void setup() 
   {
-	Ethernet.begin(mac, ip);
-	server.begin();
-	Serial.print("server is at ");
-	Serial.println(Ethernet.localIP());
-          
-    pinMode(is_on, OUTPUT);
-    dht.begin();
+    //Activating Temp sensor, webserver, LCD display, etc...
+        dht.begin();
+        Ethernet.begin(mac, ip);
+        server.begin();
 	Serial.begin(9600);
-	//boot screen
 	lcd.begin(16,2);
 	lcd.print("ArduinoStat");
-	delay(2000);
-	lcd.clear();
-	lcd.print("By: Ty");
 	delay(2000);
 	lcd.clear();
   }
 void loop()
 {
-       // lcd.clear();
+  
         //reading temperature at start of the loop
         is_temp = dht.readTemperature(true);
-        
+        light_value = analogRead(light_pin);
+        Serial.println(light_value);
         if(isnan(is_temp))
         {
           Serial.println("Sensor Failed.");
@@ -79,8 +70,16 @@ void loop()
           lcd.print("Sensor Failed.");
         }
         
-        //looking to move set temperature
-        //Button Controls for setting temperature        
+        //Button Controls for setting temperature
+        EthernetClient client = server.available();
+        if (client)
+        {
+        WaitForRequest(client);
+        ParseReceivedRequest();
+        
+        client.stop();
+        
+        }  
         while (lcd.readButtons())
         {
           lcd.clear();
@@ -88,74 +87,145 @@ void loop()
           
           if (lcd.readButtons() & BUTTON_UP)
             { 
-			  is_temp = dht.readTemperature(true);
               lcd.clear();
               delay(10);
               set_temp = set_temp + 1;
               lcd.print(set_temp);
               Serial.println(set_temp);
-              delay(250);
             }
             if (lcd.readButtons() & BUTTON_DOWN)
             {
-			  is_temp = dht.readTemperature(true);
               lcd.clear();
               delay(10);
               set_temp = set_temp - 1;
               lcd.print(set_temp);
               Serial.println(set_temp);
-              delay(250);
             }
             if (lcd.readButtons() & BUTTON_LEFT)
             {
-			  is_temp = dht.readTemperature(true);
               lcd.clear();
               set_temp = 58;
               lcd.print(set_temp);
               Serial.println(set_temp);
-              delay(250);
             }
             if (lcd.readButtons() & BUTTON_RIGHT)
-            { 
-			
-			  is_temp = dht.readTemperature(true);
+            {
               lcd.clear();
               set_temp = 66;
               lcd.print(set_temp);
               Serial.println(set_temp);
-              delay(250);
-            }
-			
-		if (lcd.readButtons() & BUTTON_SELECT)
-		{   
-			is_temp = dht.readTemperature(true);
-			lcd.clear();
-			lcd.print("Temperature: ");
-		lcd.print(is_temp);
-			lcd.println("Set Temp: ");
-			lcd.print(set_temp);
-			Serial.println("Temperature: ");
-			Serial.print(is_temp);
-			Serial.println("Set Temp: ");
-			Serial.print(set_temp);
-		}             
+            }             
         }
         //light sensor
 	//if statement to decide night time/day time
 	//resort to some sort of default temperature
-	light_value = analogRead(light_pin);
-	//Serial.println("light sensor reads ");
-        //Serial.println(light_value);
-        
-        if(set_temp > is_temp)
-        {
-          digitalWrite(is_on, HIGH);
-        }
-        
-        else
-        {
-           digitalWrite(is_on, LOW);
-        }
-  
+	light_value = analogRead(light_value);
+	Serial.print("light sensor reads ");
+	Serial.println(light_value);
+        //Check photoresistor values
+        //Look into using Date/Time as a while, to avoid so many ifs
+	if (light_value > 300)
+	{        
+                Serial.println("Probably Day Time");
+                lcd.clear();
+		lcd.print("Day Time");
+                delay(500);
+		//Set Day Temperature
+		//Mark 'day time' time
+	}
+	else
+	{
+		Serial.println("Probably Night Time");
+                lcd.clear();
+		lcd.print("Night Time");
+                delay(500);
+		//Set Night Temperature
+		//Mark 'bed time' time
+	
+      
+     // PrintNumber("bufferSize", bufferSize);
+
+
+}
+}
+    void WaitForRequest(EthernetClient client) // Sets buffer[] and bufferSize
+    {
+      bufferSize = 0;
      
+      while (client.connected()) 
+        {
+        if (client.available())
+           {
+            char c = client.read();
+            if (c == '\n')
+              break;
+            else
+              if (bufferSize < bufferMax)
+                buffer[bufferSize++] = c;
+              else
+                break;
+           }
+        }
+        
+    }
+void ParseReceivedRequest()
+    {
+      Serial.println("in ParseReceivedRequest");
+      Serial.println(buffer);
+      
+      //Received buffer contains "GET /cmd/param1/param2 HTTP/1.1".  Break it up.
+      char* slash1;
+      char* slash2;
+      char* slash3;
+      char* space2;
+      
+      slash1 = strstr(buffer, "/") + 1; // Look for first slash
+      slash2 = strstr(slash1, "/") + 1; // second slash
+      slash3 = strstr(slash2, "/") + 1; // third slash
+      space2 = strstr(slash2, " ") + 1; // space after second slash (in case there is no third slash)
+      if (slash3 > space2) slash3=slash2;
+    
+      PrintString("slash1",slash1);
+      PrintString("slash2",slash2);
+      PrintString("slash3",slash3);
+      PrintString("space2",space2);
+      
+      // strncpy does not automatically add terminating zero, but strncat does! So start with blank string and concatenate.
+      cmd[0] = 0;
+      param1[0] = 0;
+      param2[0] = 0;
+      strncat(cmd, slash1, slash2-slash1-1);
+      strncat(param1, slash2, slash3-slash2-1);
+      strncat(param2, slash3, space2-slash3-1);
+      
+      PrintString("cmd",cmd);
+      PrintString("param1",param1);
+      PrintString("param2",param2);
+    }
+    
+    void PrintString(char* label, char* str)
+{
+  Serial.print(label);
+  Serial.print("=");
+  Serial.println(str);
+}
+
+void PrintNumber(char* label, int number)
+{
+  Serial.print(label);
+  Serial.print("=");
+  Serial.println(number, DEC);
+  
+}
+  void PerformRequestedCommands()
+{
+  //Edit all of this to fit setting the temperature of things.
+  if ( strcmp(cmd,"digitalWrite") == 0 )
+  {
+   RemoteDigitalWrite();
+  }
+  if ( strcmp(cmd,"analogRead") == 0 )
+   {
+     RemoteAnalogRead();
+   }
 }
